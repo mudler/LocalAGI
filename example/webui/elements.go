dbfc59633310275c8e99182b3e007ffb59e74023
mudler <mudler@localai.io>
2024-04-07 20:30:20 +0200

    comment

--- example/webui/elements.go
+++ example/webui/elements.go
@@ -2,6 +2,8 @@ package main
 
 import "fmt"
 
+// TODO: switch to https://github.com/chasefleming/elem-go
+
 func chatDiv(content string, color string) string {
 	return fmt.Sprintf(`<div class="p-2 my-2 rounded bg-%s-600">%s</div>`, color, htmlIfy(content))
 }
