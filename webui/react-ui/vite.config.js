62e08bf9006024f0f3c0639459fb0873a37036ad
Richard Palethorpe <io@richiejp.com>
2025-04-13 14:57:50 +0100

    fix(ui): Proxy avatars endpoint in dev mode
    Signed-off-by: Richard Palethorpe <io@richiejp.com>

--- webui/react-ui/vite.config.js
+++ webui/react-ui/vite.config.js
@@ -30,6 +30,7 @@ export default defineConfig(({ mode }) => {
         '/status': backendUrl,
         '/action': backendUrl,
         '/actions': backendUrl,
+        '/avatars': backendUrl
       }
     }
   }
